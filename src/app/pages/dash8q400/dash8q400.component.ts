e2c4b4278f3effddd861627844b09f5746412284
Mingze Gao <mgao14@sheffield.ac.uk>
2020-04-02 18:26:35 +0800

    Function: Dash8-Q400 Status: Stage II

--- src/app/pages/dash8q400/dash8q400.component.ts
+++ src/app/pages/dash8q400/dash8q400.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { cal_takeoff, cal_landing } from './performance';
 
 @Component({
   selector: 'app-dash8q400',
@@ -7,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Dash8q400Component implements OnInit {
   departure = "";
+  departure_oat = "18";
   arrival = "";
-  flapValue = "5";
+  arrival_oat = "";
+  takeoff_flapValue = "5";
+  altitude = "5000";
 
-  ZFW = "";
+  ZFW = "17.000";
   ALTN = "";
   RESV = "";
   CONT = "";
@@ -18,6 +22,47 @@ export class Dash8q400Component implements OnInit {
   DOI = "94.6";
   TAKEOFF = "";
   TRIP = "";
+
+  V1 = "---";
+  Vr = "---";
+  V2 = "---";
+  V_FRI = "---";
+  V_CLIMB = "---";
+  V_CRUISE = "---";
+
+  landing_flapValue = "35";
+  V_app = "---";
+  V_ref = "---";
+  V_ga = "---";
+
+  onCalculate(mode): void {
+    switch (mode){
+      case 'takeoff':
+        {
+          var temp, Vr, V2, V_FRI, V_CLIMB;
+          [temp, V_FRI, V_CLIMB] = cal_takeoff(this.takeoff_flapValue, this.departure_oat, this.altitude, this.ZFW);
+          [Vr, V2] = temp;
+
+          this.V1 = Vr;
+          this.Vr = Vr;
+          this.V2 = V2;
+          this.V_FRI = V_FRI;
+          this.V_CLIMB = V_CLIMB;
+          break;
+        }
+      case 'landing':
+        {
+          var V_app, V_ref, V_ga;
+          [V_app, V_ref, V_ga] = cal_landing(this.landing_flapValue, this.ZFW);
+
+          this.V_app = V_app;
+          this.V_ref = V_ref;
+          this.V_ga = V_ga;
+          break;
+        }
+    }
+
+  }
 
   constructor() { }
 
